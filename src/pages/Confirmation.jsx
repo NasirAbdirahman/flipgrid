d720c75fce5b4cd9c5cf73bbc8b11e8467b39a67
AbdulNasir Abdirahman <nasir.a.abdirahman@outlook.com>
2022-03-02 10:36:17 -0800

    Created Routing and inital Signup and confirmation Page Skeletons

---
+++ src/pages/Confirmation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Confirmation = () => {
+    return(
+        <div>
+            <header>Confirmation page</header>
+        </div>
+    )
+}
+
+export default Confirmation
